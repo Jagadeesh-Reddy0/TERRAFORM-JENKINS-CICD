179838114cf9a5aece29409f707fd091cc600b30
Jagadeesh-Reddy0 <65006534+Jagadeesh-Reddy0@users.noreply.github.com>
2023-11-02 13:14:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,5 +17,5 @@ variable "ami_id" {
 variable "bucketname" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "test-jenkins-cloud"
+  default     = "test-jenkins-cloud-2023"
 }
